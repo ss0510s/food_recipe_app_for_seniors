ee3d03410de659ab1f9af0584be41eb768b12cbc
ss0510s <77006790+ss0510s@users.noreply.github.com>
2021-06-29 22:33:13 +0900

    Update EffectActivity.java

--- app/src/main/java/com/example/project2/EffectActivity.java
+++ app/src/main/java/com/example/project2/EffectActivity.java
@@ -14,11 +14,14 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
+//효능에 따른 분류 화면
 public class EffectActivity extends AppCompatActivity implements MainAdapter.onItemListener{
     androidx.appcompat.widget.Toolbar toolbar;
     RecyclerView recyclerView;
     RecyclerView.LayoutManager linearLayoutManager; //리니어 레이아웃
     MainAdapter adapter;// Adapter adapter;
+
+    //아이템을 저장할 리스트
     private ArrayList<ItemList> itemList=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,13 +33,14 @@ public class EffectActivity extends AppCompatActivity implements MainAdapter.onI
         setSupportActionBar(toolbar);  //툴바를 액션바로 지정
         getSupportActionBar().setDisplayShowCustomEnabled(true);    //툴바를 보여줌
         getSupportActionBar().setDisplayShowTitleEnabled(false);    //기본 제목을 없애줌
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);  //home화면으로 이동하는 버튼
         title.setText("효능");
 
         setUpRecyclerView();
     }
-    private void setUpRecyclerView() {
 
+    //리사이클러뷰로 아이템들을 보여줌
+    private void setUpRecyclerView() {
         itemList = new ArrayList<>();
         recyclerView = findViewById(R.id.recyclerView); //리사이클러뷰 참조
         //리니어 레이아웃
@@ -49,6 +53,7 @@ public class EffectActivity extends AppCompatActivity implements MainAdapter.onI
         adapter.setOnClickListener(this);
     }
 
+    //아이템 리스트
     private void fillData(){
         itemList = new ArrayList<>();
         itemList.add(new ItemList(R.drawable.blood,"혈액순환"));
@@ -60,6 +65,8 @@ public class EffectActivity extends AppCompatActivity implements MainAdapter.onI
         itemList.add(new ItemList(R.drawable.liver,"간"));
         itemList.add(new ItemList(R.drawable.stomach,"위장"));
     }
+
+    //메뉴 생성
     public boolean onCreateOptionsMenu(Menu menu) {
 
         getMenuInflater().inflate(R.menu.menu, menu);
@@ -82,8 +89,10 @@ public class EffectActivity extends AppCompatActivity implements MainAdapter.onI
         });
         return super.onCreateOptionsMenu(menu);
     }
+    //아이템 클릭시 효과
     public void onItemClicked(int position) {
         Intent intent=new Intent(EffectActivity.this, EffectActivity2.class);
+        //인수를 넘겨줌
         intent.putExtra("effect", position);
         startActivity(intent);
     }
